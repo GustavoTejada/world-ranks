ee6e0ad03c0cab475b5a988ef6e8387815d0e54f
Gustavo <gtejada@travelpaq.com.ar>
2021-01-11 23:06:04 -0300

    prueba

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,19 +1,16 @@
 import { KeyboardArrowDownRounded } from "@material-ui/icons";
 import { KeyboardArrowUpRounded } from "@material-ui/icons";
 import { useState } from "react";
+import Link from "next/Link";
 import styles from "./CountriesTable.module.css";
 
 const orderBy = (countries, value, direction) => {
   if (direction === "asc") {
-    return [...countries].sort((a, b) =>
-      a.[value] > b.[value] ? 1 : -1
-    );
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
   }
 
   if (direction === "desc") {
-    return [...countries].sort((a, b) =>
-      a.[value] > b.[value] ? -1 : 1
-    );
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
   }
 
   return countries;
@@ -63,29 +60,46 @@ const CountriesTable = ({ countries }) => {
   return (
     <div>
       <div className={styles.heading}>
+        <div className={styles.heading_flag}></div>
         <button
           className={styles.heading_name}
           onClick={() => setValueAndDirection("name")}
         >
           <div>Name</div>
 
-          <SortArrow direction={direction} />
+          {value ==='name' && <SortArrow direction={direction} />}
         </button>
+
         <button
           className={styles.heading_population}
           onClick={() => setValueAndDirection("population")}
         >
           <div>Population</div>
 
-          <SortArrow direction={direction} />
+          {value ==='population' && <SortArrow direction={direction} />}
+        </button>
+
+        <button
+          className={styles.heading_area}
+          onClick={() => setValueAndDirection("area")}
+        >
+          <div>Area (km<sup style={{fontSize: '0.5rem'}}>2</sup>)</div>
+
+          {value ==='area' && <SortArrow direction={direction} />}
         </button>
       </div>
 
       {orderedCountries.map((country) => (
-        <div className={styles.row}>
-          <div className={styles.name}>{country.name}</div>
-          <div className={styles.population}>{country.population}</div>
-        </div>
+        <Link href={`/country/${country.alpha3Code}`} key={country.name}>
+          <div className={styles.row}>
+            <div className={styles.flag}>
+              <img src={country.flag} alt={country.name}></img>
+            </div>
+            <div className={styles.name}>{country.name}</div>
+            <div className={styles.population}>{country.population}</div>
+            <div className={styles.area}>{country.area}</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
